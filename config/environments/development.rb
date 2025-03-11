2c13f7e09d9790d27ab241fa4fb20436a82d275d
Owaru Ryudo <ryudoawaru@gmail.com>
2025-03-11 17:56:49 +0800

    Use cable & queue in dev mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,6 +54,8 @@ Rails.application.configure do
 
   # Highlight code that enqueued background job in logs.
   config.active_job.verbose_enqueue_logs = true
+  config.active_job.queue_adapter = :solid_queue
+  config.solid_queue.connects_to = { database: { writing: :queue } }
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
@@ -64,6 +66,7 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
+  # config.action_cable.adapter = :solid_cable
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
 
